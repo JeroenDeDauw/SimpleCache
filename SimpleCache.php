70b99fc37aa628b3614a5e14c2311b939cca39c5
jeroendedauw <jeroendedauw@gmail.com>
2013-07-06 00:19:42 +0200

    bump to 1.0

--- SimpleCache.php
+++ SimpleCache.php
@@ -12,7 +12,7 @@ if ( defined( 'SimpleCache_VERSION' ) ) {
 	return;
 }
 
-define( 'SimpleCache_VERSION', '0.1' );
+define( 'SimpleCache_VERSION', '1.0' );
 
 // @codeCoverageIgnoreStart
 spl_autoload_register( function ( $className ) {
